bb98c455d8e19f8520b00d8b0634e05884a6651f
Giannis Fakinos <giannhs.afasias@gmail.com>
2021-03-10 23:26:42 +0200

    q3 csv/parquet

---
+++ src/query3.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from pyspark.sql import SparkSession
+import sys
+
+def query(format):
+    spark = SparkSession.builder.appName('query1-sql').getOrCreate()
+
+    if format == "csv":
+        df = spark.read.format("csv").option("header", "true")
+    elif format == "parquet":
+        df = spark.read.format("parquet")
+    else:
+        print("No such format available!")
+        exit(-1)
+
+    df1 = df.load("/user/data/ratings." + format)
+    df1.registerTempTable("ratings")
+    df2 = df.load("/user/data/movie_genres." + format)
+    df2.registerTempTable("movie_genres")
+
+    sqlString = \
+    "select mg.genre, AVG(r.rating) as mean_rating, count(distinct(mg.movie_id)) as movies " + \
+	"From movie_genres as mg, ratings as r " + \
+	"Where mg.movie_id == r.movie_id " + \
+    "Group by mg.genre"
+    
+    # Query
+    spark.sql(sqlString).show()
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Please provide one file format form the above:")
+        print("(1) csv, (2) parquet")
+        exit(0)
+    else:
+        query(sys.argv[1])
