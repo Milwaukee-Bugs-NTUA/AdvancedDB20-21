4a85ee9428907fafa2d8cef6a46f6852031cce8a
Giannis Fakinos <giannhs.afasias@gmail.com>
2021-03-11 21:37:29 +0200

    q5 done

--- src/query5.py
+++ src/query5.py
@@ -18,6 +18,8 @@ def query4(format):
     df1.registerTempTable("ratings")
     df2 = df.load("/user/data/movie_genres." + format)
     df2.registerTempTable("movie_genres")
+    df3 = df.load("/user/data/movies." + format)
+    df3.registerTempTable("movies")
 
     sqlString = \
     """
@@ -28,18 +30,94 @@ def query4(format):
             where mg.movie_id = r.movie_id
             group by mg.genre,r.user_id 
             grouping sets ((mg.genre,r.user_id))
-        )
-    select t1.genre,first(t2.user_id) as user_id,first(t1.max_num) as number_of_reviews
-    from 
+        ),
+    special_users(genre,user_id,max_num) as
+    (
+        select t1.genre,first(t2.user_id) as user_id,first(t1.max_num) as number_of_reviews
+        from 
+            (
+                select genre, max(num) as max_num
+                from ratings_per_user_genre
+                group by genre
+            ) as t1 inner join 
+            ratings_per_user_genre as t2
+        on t1.genre = t2.genre and t1.max_num = t2.num
+        group by t1.genre
+        order by t1.genre
+    ),
+    special_users_ratings(genre,user_id,max_num,rating,movie_id,title,popularity) as
+    (
+        select s.genre,s.user_id,s.max_num, rating, m.movie_id, m.title, m.popularity
+        from special_users as s, ratings as r, movies as m, movie_genres as mg
+        where s.user_id = r.user_id
+            and r.movie_id = m.movie_id
+            and m.movie_id = mg.movie_id
+            and s.genre = mg.genre
+    ),
+    max_ratings_table(genre,user_id,max_num,movie_id,title,max_rating,popularity) as
+    (
+        select t1.genre,t1.user_id,t1.max_num,t2.movie_id,t2.title,t1.max_rating,t2.popularity
+        from 
+            (
+                select genre,user_id,max_num,max(rating) as max_rating
+                from special_users_ratings
+                group by genre,user_id,max_num
+                grouping sets ((genre,user_id,max_num))
+            ) as t1 inner join 
+            special_users_ratings as t2 on
+        t1.genre = t2.genre and
+        t1.user_id = t2.user_id and
+        t1.max_rating = t2.rating
+    ),
+    min_ratings_table(genre,user_id,max_num,movie_id,title,min_rating,popularity) as
+    (
+        select t1.genre,t1.user_id,t1.max_num,t2.movie_id,t2.title,t1.min_rating,t2.popularity
+        from 
+            (
+                select genre,user_id,max_num,min(rating) as min_rating
+                from special_users_ratings
+                group by genre,user_id,max_num
+                grouping sets ((genre,user_id,max_num))
+            ) as t1 inner join 
+            special_users_ratings as t2 on
+        t1.genre = t2.genre and
+        t1.user_id = t2.user_id and
+        t1.min_rating = t2.rating
+    )
+    select favourite.genre,favourite.user_id,favourite.max_num,
+            favourite.title,favourite.max_rating,least_favourite.title,least_favourite.min_rating
+    from
         (
-            select genre, max(num) as max_num
-            from ratings_per_user_genre
-            group by genre
-        ) as t1 inner join 
-        ratings_per_user_genre as t2
-    on t1.genre = t2.genre and t1.max_num = t2.num
-    group by t1.genre
-    order by t1.genre
+            select t1.genre,t1.user_id,t1.max_num,t2.title,t1.max_rating
+            from
+                (
+                    select genre,user_id,max_num,max_rating,max(popularity) as max_popularity
+                    from max_ratings_table
+                    group by genre,user_id,max_num,max_rating
+                    grouping sets ((genre,user_id,max_num,max_rating))
+                ) as t1 inner join
+                max_ratings_table as t2 on
+            t1.genre = t2.genre and
+            t1.user_id = t2.user_id and
+            t1.max_popularity = t2.popularity
+        ) as favourite,
+        (
+            select t1.genre,t1.user_id,t1.max_num,t2.title,t1.min_rating
+            from
+                (
+                    select genre,user_id,max_num,min_rating,max(popularity) as max_popularity
+                    from min_ratings_table
+                    group by genre,user_id,max_num,min_rating
+                    grouping sets ((genre,user_id,max_num,min_rating))
+                ) as t1 inner join
+                min_ratings_table as t2 on
+            t1.genre = t2.genre and
+            t1.user_id = t2.user_id and
+            t1.max_popularity = t2.popularity
+        ) as least_favourite
+    where favourite.genre = least_favourite.genre
+    and favourite.user_id = least_favourite.user_id
+    order by favourite.genre
     """
 
     # Query
