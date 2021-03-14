99fa05baf994f74e20cadd0603ae7dd354416376
Giannis Fakinos <giannhs.afasias@gmail.com>
2021-03-14 11:21:06 +0200

    WIP: q5 with special users in filter

--- src/query5_rdd.py
+++ src/query5_rdd.py
@@ -22,12 +22,23 @@ def query5_rdd():
         mapPartitionsWithIndex(ignore_header). \
         map(lambda x: (int(split_complex(x)[0]), split_complex(x)[1]))
     # (movie_id, genre)
+    # dummy_genres = \
+    #     sc.textFile("hdfs://master:9000/user/data/movie_genres.csv"). \
+    #     mapPartitionsWithIndex(ignore_header). \
+    #     map(lambda x: ((int(split_complex(x)[0]),split_complex(x)[1]), ()))
+    # # ((movie_id,genre),())
         
     ratings = \
         sc.textFile("hdfs://master:9000/user/data/ratings.csv"). \
         mapPartitionsWithIndex(ignore_header). \
         map(lambda x: (int(split_complex(x)[1]), (int(split_complex(x)[0]), float(split_complex(x)[2]))))
     # (movie_id, (user_id, rating))
+
+    # user_ratings = \
+    #     sc.textFile("hdfs://master:9000/user/data/ratings.csv"). \
+    #     mapPartitionsWithIndex(ignore_header). \
+    #     map(lambda x: (int(split_complex(x)[0]), (int(split_complex(x)[1]), float(split_complex(x)[2]))))
+    # # (user_id, (movie_id, rating))
     
     movies = \
         sc.textFile("hdfs://master:9000/user/data/movies.csv"). \
@@ -48,18 +59,45 @@ def query5_rdd():
     # map ((genre,user_id), 1)
     # reduce ((genre,user_id),num)
     # map (genre, (user_id,num))
+    # special users ar computed
+    # map (user_id, (genre,num))
+    # join (user_id, (genre,num)) x (user_id,(movie_id,rating))
+    # (user_id, (genre,num),(movie_id, rating))
+    # mapvalues (user_id, (genre,num, movie_id, rating))
+    # map ((movie_id,genre), (num, user_id,rating))
+    # join ((movie_id,genre), (num, user_id,rating)) X ((movie_id,genre),())
     special_users = \
         genres.join(ratings). \
         map(lambda x: ((x[1][0],x[1][1][0]), 1)). \
         reduceByKey(lambda x, y: x + y). \
         map(lambda x: (x[0][0], (x[0][1],x[1]))). \
         reduceByKey(lambda x, y: x if x[1] > y[1] else y). \
+        map(lambda x: (x[1][0], (x[0],x[1][1]))). \
         collect()
 
-        # ((u,genre), (num, movie_id, rating, title, popularity))
+    print("Special Users computed")
+    special = dict(special_users)
+
+    special_ratings = \
+        ratings.filter(lambda x: x[1][0] in special). \
+        take(10)      
+    
+    print("Special ratings computed")
+        
+        
+        # map(lambda x: (x[1][0], (x[0],x[1][1]))). \
+        # join(user_ratings). \
+        # mapValues(lambda t1: t1[0] + t1[1]). \
+        # map(lambda x: ((x[1][0],x[1][2]), (x[1][1],x[0],x[1][3]))). \
+        # join(dummy_genres). \
+        # filter(lambda x: x[0][1] == 'Action'). \
+        # take(10)        
+
     end = time.time()
 
     for i in special_users:
+        print(i)
+    for i in special_ratings:
         print(i)
     print("Execution time:",end - start,"secs")
 
