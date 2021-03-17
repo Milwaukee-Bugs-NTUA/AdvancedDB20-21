15718662c8ff2f8604ab33268de04f2de44b6346
Giannis Fakinos <giannhs.afasias@gmail.com>
2021-03-17 15:26:00 +0200

    Broadcast join

--- src/broadcast_join.py
+++ src/broadcast_join.py
@@ -19,26 +19,44 @@ if __name__ == "__main__":
     spark = SparkSession.builder.appName('broadcast-join').getOrCreate()
     sc = spark.sparkContext
     
+    start = time.time()
     movie_genres = \
-        sc.textFile("hdfs://master:9000/user/data/movie_genres.csv"). \
+        sc.textFile("hdfs://master:9000/user/data/movie_genres_100.csv"). \
         mapPartitionsWithIndex(ignore_header). \
         map(split_complex). \
         map(lambda row:(row[0],row[1])). \
         groupByKey(). \
         map(lambda row: (row[0],tuple(row[1]))). \
-        take(2)
+        collect()
 
     movie_genres_dict = dict(movie_genres)
     sc.broadcast(movie_genres_dict)
+    
+    def helper(iterator):
+        global movie_genres_dict
+        res = []
+        for row in iterator:
+            row = split_complex(row)
+            if row[1] in movie_genres_dict:
+                res.append(row)
+        return iter(res)
+    
+    def join(iterator):
+        global movie_genres_dict
+        res = []
+        for row in iterator:
+            for genre in movie_genres_dict[row[1]]:
+                res.append((row[1],(genre, (row[0],row[2],row[3]))))
+        return iter(res)
 
-    # Start join
     ratings = \
         sc.textFile("hdfs://master:9000/user/data/ratings.csv"). \
         mapPartitionsWithIndex(ignore_header). \
-        map(lambda x: (split_complex(x)[1], (split_complex(x)[0], split_complex(x)[2], split_complex(x)[3]))). \
-        filter(lambda x: x[0] in movie_genres_dict). \
-        map(lambda x: (x[0], (movie_genres_dict[x[0]],x[1]))). \
+        mapPartitions(helper). \
+        mapPartitions(join). \
         collect()
+    end = time.time()
 
-    for i in ratings:
-        print(i)
+    # for i in ratings:
+    #     print(i)
+    print("Execution time with broadcast join: {} secs".format(end-start))
