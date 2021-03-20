35360d116c353b8169709b9d659013f73322dcdb
Giannis Fakinos <giannhs.afasias@gmail.com>
2021-03-20 13:39:45 +0200

    Convert to parquet format first 100 rows of movie_genres

--- src/convert.py
+++ src/convert.py
@@ -8,6 +8,8 @@ df_movies.to_parquet('../dataset/movies.parquet')
 # genres
 df_genres = pd.read_csv('../dataset/movie_genres.csv')
 df_genres.to_parquet('../dataset/movie_genres.parquet')
+df_genres = pd.read_csv('../dataset/movie_genres_100.csv')
+df_genres.to_parquet('../dataset/movie_genres_100.parquet')
 # rankings
 df_rankings = pd.read_csv('../dataset/ratings.csv')
 df_rankings.to_parquet('../dataset/ratings.parquet')
